60dcd6bde89541a69136cb81554b307b576db345
AlvaroQRS <dainer10mta@gmail.com>
2019-04-17 20:01:31 -0500

    añdi un nuevo section de lugares populares

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  autoplayTimeout:5000,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:1
-      },
-      1000:{
-          items:1
-      }
-  }
-})
+$('.active-gallery-carousel').owlCarousel({
+    items: 2,
+    // autoplay: 2500,
+    loop: true,
+    margin: 30,
+    nav: true,
+    navText: [ "<img src='img/cprev.png'>", "<img src='img/cnext.png'>" ],
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        420: {
+            items: 1
+        },
+        575: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1200: {
+            items: 2
+        },
+        1680: {
+            items: 2
+        }
+    }
+});
